65776adf6a53e56ceea810c67f1c715a3e6451d0
Tzuk <tzuk.asaf10@gmail.com>
2022-02-24 18:23:10 +0200

    made the compass rose but without the logic behind

--- src/BoardFrame.java
+++ src/BoardFrame.java
@@ -1,8 +1,12 @@
 import Backend.BoardState;
 import enums.Player;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Vector;
 
 public class BoardFrame extends JFrame implements MarbleListener
@@ -15,7 +19,9 @@ public class BoardFrame extends JFrame implements MarbleListener
     private Player winner;
     private JButton reset;
     private JButton exit;
+    private JButton right, left, upright, upleft, downright, downleft;
     private  BoardState ab;
+    private ArrayList<Point> selected;// the marbles that the player has selected
     private int width,height;
 
     /**
@@ -24,8 +30,7 @@ public class BoardFrame extends JFrame implements MarbleListener
      * @param numOfColsInFirstRow
      * @param ab
      */
-    public BoardFrame(int numOfRows, int numOfColsInFirstRow, BoardState ab)
-    {
+    public BoardFrame(int numOfRows, int numOfColsInFirstRow, BoardState ab){
 
         setLayout(null);
         width=1000;
@@ -35,11 +40,20 @@ public class BoardFrame extends JFrame implements MarbleListener
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         bp=new BoardPanel(numOfRows,numOfColsInFirstRow,this);
         this.ab = ab;
-
+        selected = new ArrayList<>();
         add(bp);
         bp.setBounds(0, 0, width, height);
         source = dest = null;
         winner = null;
+
+        left = new JButton();
+        right = new JButton();;
+
+        downright = new JButton();
+        downleft = new JButton();
+        upright = new JButton();
+        upleft = new JButton();
+        setButtons();
     }
 
 
@@ -60,6 +74,68 @@ public class BoardFrame extends JFrame implements MarbleListener
         }
     }
 
+    public void setButtons()
+    {
+        try
+        {
+            right.setIcon(new ImageIcon(ImageIO.read(Objects.requireNonNull(getClass().getResource("Assets/right.png")))));
+            left.setIcon(new ImageIcon(ImageIO.read(Objects.requireNonNull(getClass().getResource("Assets/left.png")))));
+            upright.setIcon(new ImageIcon(ImageIO.read(Objects.requireNonNull(getClass().getResource("Assets/upright.png")))));
+            downright.setIcon(new ImageIcon(ImageIO.read(Objects.requireNonNull(getClass().getResource("Assets/downright.png")))));
+            upleft.setIcon(new ImageIcon(ImageIO.read(Objects.requireNonNull(getClass().getResource("Assets/upleft.png")))));
+            downleft.setIcon(new ImageIcon(ImageIO.read(Objects.requireNonNull(getClass().getResource("Assets/downleft.png")))));
+
+        } catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+        right.setOpaque(false);
+        right.setContentAreaFilled(false);
+        right.setBounds(860,250,90,90);
+        right.setLayout(null);
+        right.setVisible(true);
+
+        left.setOpaque(false);
+        left.setContentAreaFilled(false);
+        left.setBounds(770,250,90,90);
+        left.setLayout(null);
+        left.setVisible(true);
+
+        upright.setOpaque(false);
+        upright.setContentAreaFilled(false);
+        upright.setBounds(860,160,90,90);
+        upright.setLayout(null);
+        upright.setVisible(true);
+
+        downright.setOpaque(false);
+        downright.setContentAreaFilled(false);
+        downright.setBounds(860,340,90,90);
+        downright.setLayout(null);
+        downright.setVisible(true);
+
+        upleft.setOpaque(false);
+        upleft.setContentAreaFilled(false);
+        upleft.setBounds(770,160,90,90);
+        upleft.setLayout(null);
+        upleft.setVisible(true);
+
+        downleft.setOpaque(false);
+        downleft.setContentAreaFilled(false);
+        downleft.setBounds(770,340,90,90);
+        downleft.setLayout(null);
+        downleft.setVisible(true);
+
+
+
+        bp.add(right);
+        bp.add(upright);
+        bp.add(left);
+        bp.add(downright);
+        bp.add(upleft);
+        bp.add(downleft);
+        repaint();
+    }
+
 
     /**
      * the function is in charge of managing the event of clicking a marble
@@ -68,8 +144,22 @@ public class BoardFrame extends JFrame implements MarbleListener
     @Override
     public void MarbleSelected(Point pos)
     {
-
-        bp.get_panels()[pos.y][pos.x].markMarble();
+        if(selected.contains(pos))
+        {
+            /*
+            if the player clicked a marble that is already selected
+            then cancel the selection of that marble
+            */
+            selected.remove(pos);
+            bp.get_panels()[pos.y][pos.x].drawMarble();
+        }
+        else
+        {
+            //the selected marble hasn't been selected yet
+            bp.get_panels()[pos.y][pos.x].markMarble();
+            selected.add(pos);
+        }
+        System.out.println(selected);
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,6 @@ public class Main extends JFrame
         af.setResizable(false);
         af.setBoard();
 
+
     }
 }
