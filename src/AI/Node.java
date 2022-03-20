39ca4d43df5889ece6deea2346ff4cbd84ded7b1
Tzuk <tzuk.asaf10@gmail.com>
2022-03-20 13:51:47 +0200

    Created the Node class

--- src/AI/Node.java
+++ src/AI/Node.java
@@ -1,6 +1,37 @@
 package AI;
 
+import Backend.BoardStructure;
+
+import java.util.ArrayList;
+
 public class Node
 {
+    private BoardStructure board;
+    private ArrayList<Node> children;
 
+    public Node(BoardStructure board)
+    {
+        this.board = board;
+        this.children = new ArrayList<>();
+    }
+
+    public void AddChild(Node child)
+    {
+        this.children.add(child);
+    }
+
+    public BoardStructure GetBoard()
+    {
+        return board;
+    }
+
+    public ArrayList<Node> GetChildren()
+    {
+        return children;
+    }
+
+    public boolean HasChildren()
+    {
+        return !this.children.isEmpty();
+    }
 }
