b7e2d31cdd6af32743b020700a23d5079a0cf92f
Tzuk <tzuk.asaf10@gmail.com>
2022-04-06 18:19:02 +0300

    The evaluation needs to know the player

--- src/AI/Evaluate.java
+++ src/AI/Evaluate.java
@@ -2,9 +2,11 @@ package AI;
 
 import Backend.BoardManage;
 import Backend.BoardStructure;
+import GUI.Marble;
 import enums.Player;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Evaluate
@@ -16,21 +18,23 @@ public class Evaluate
     private int pushOut = 200;// if the move will get the AI to push an enemy's marble out of the board
     private int dangerous = -200;// if the move will cause that an AI's marble is pushable by enemy's marbles
     private BoardManage boardManage;
+    private BoardStructure dataStructure;
 
     public Evaluate(BoardStructure dataStructure, BoardManage boardManage)
     {
+        this.dataStructure = dataStructure;
     }
 
-    public int BallsDistancesToEdge(BoardStructure dataStructure)
+    public int BallsDistancesToEdge(BoardStructure dataStructure, Player player)
     {
 
         //System.out.println(dataStructure.numOfBlacks - dataStructure.numOfWhites);
-        return dataStructure.numOfBlacks - dataStructure.numOfWhites;
+        return (dataStructure.numOfBlacks - dataStructure.numOfWhites);
     }
 
-    public int evaluateAllBoard(BoardStructure dataStructure)
+    public int evaluateAllBoard(BoardStructure dataStructure, Player p)
     {
-        return BallsDistancesToEdge(dataStructure);
+        return BallsDistancesToEdge(dataStructure, p);
 
     }
 
